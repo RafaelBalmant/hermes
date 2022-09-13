a0632b982ef4779f53a3692eefa68645ab2719f4
rafaelbb <rafael.balmant@afya.com.br>
2022-09-13 13:30:36 -0300

    updated

--- src/modules/users/functionalities.ts
+++ src/modules/users/functionalities.ts
@@ -4,7 +4,6 @@ import { inject, injectable } from "tsyringe";
 import { ILogsRepository } from "../shared/logs/respositories/ILogsRepository";
 import { compare, hash } from "bcrypt";
 import { sign } from "jsonwebtoken";
-import { promises } from "dns";
 
 @injectable()
 export class UserFunctionalities {
